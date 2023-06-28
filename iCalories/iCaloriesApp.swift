e5985bd09242173e09abb0a5f43d72fd2bc3681d
BI004709 <doni.silva@bancointer.com>
2023-06-28 13:13:03 -0300

    corrigindo alteração realizada

--- iCalories/iCaloriesApp.swift
+++ iCalories/iCaloriesApp.swift
@@ -10,7 +10,6 @@ import SwiftUI
 @main
 struct iCaloriesApp: App {
     @StateObject private var dataController = DataController()
-    @StateObject private var dataController2 = DataController()
     
     var body: some Scene {
         WindowGroup {
