b472ab8e0b53076383f0a8934c5ef97c2118221c
BI004709 <doni.silva@bancointer.com>
2023-06-28 12:57:34 -0300

    commit inicial - adicionar arquivos do projeto

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,20 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Doni Ramos da Silva on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
